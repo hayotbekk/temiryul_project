9787a868aa7fe35c3c6617ade37049c66a98a983
amirbekk7 <normurodovamirbek730@gmail.com>
2024-05-26 14:01:22 +0300

    components finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from './components/header'
+import Hero from './components/hero'
+import Footer from './components/footter'
 
 const App = () => {
   return (
     <div>
       <Header/>
+      <Hero/>
+      <Footer/>
     </div>
   )
 }
